6deb60d0e0c6cc81f93a3a70bb301a0f7db1dd14
Chi Tang V.Q <tvquocchi@gmail.com>
2017-07-26 08:57:14 +0700

    Angular Day 2

--- Angular/my-app/src/app/app.module.ts
+++ Angular/my-app/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
+import { GetMember } from './getmem.component';
+import { ListMember } from './listmem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetMember,
+    ListMember
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
